e2b9dd969fd55bfa4efdaed159f668c267c9e2dd
zwitaagustina <zwitaagustinasianturi3536@gmail.com>
2025-06-06 14:57:50 +0700

    buat halaman dashboard

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => '240296',
+        'database'     => 'proyek',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'home::index');
+$routes->get('/dasboard', 'DasboardController::index');
