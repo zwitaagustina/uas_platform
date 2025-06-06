b5c9a594579502225ca46351206fb775a9d769ee
zwitaagustina <zwitaagustinasianturi3536@gmail.com>
2025-06-07 03:24:50 +0700

    Menambahkan Produk

--- app/Controllers/DasboardController.php
+++ app/Controllers/DasboardController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controllers;
 
-//use App\Models\AsistenModel;
+use App\Models\ModelBarang;
 
 class DasboardController extends BaseController
 {
     public function index()
     {
+        $model = new ModelBarang();
+
+        $data = [
+            'judul' => 'barang',
+            'barang' => $model->tampil_data()
+        ];
         echo view('templates/header');
         echo view('templates/sidebar');
-        echo view('dasboard');
+        echo view('dasboard', $data);
         echo view('templates/footer');
     }
 }
---
+++ app/Models/ModelBarang.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelBarang extends Model
+{
+    protected $table = 'products'; 
+    protected $primaryKey = 'product_id'; 
+    protected $allowedFields = ['nama_produk', 'deskripsi', 'harga', 'stok', 'gambar', 'kategori']; 
+
+    public function tampil_data()
+    {
+        return $this->findAll(); 
+    }
+}
